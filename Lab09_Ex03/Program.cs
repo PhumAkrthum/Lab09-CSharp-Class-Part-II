adc88d20feb17295fc4f2b28560c6899ac460718
PhumAkrthum <66030270@kmitl.ac.th>
2025-02-08 17:45:05 +0700

    Update3

---
+++ Lab09_Ex03/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Person.name = "Phum Akrthum"; 
+System.Console.WriteLine(Person.Getname());
+var student = new Student();
+Student.name = "Phum Akrthum";
+System.Console.WriteLine(student.Getname());
+
+static class Person
+{
+    static public string? name;
+    static public string Getname()
+    {
+        return $"Hello from {name}"; 
+    }
+}
+
+class Student
+{
+    public static string? name; 
+    public string Getname()
+    {
+        return $"Hello from {name}"; 
+    }
+}
