0285f8cdd35c8abad019a42fc177a2f0ebf32e8c
PhumAkrthum <66030270@kmitl.ac.th>
2025-02-08 22:10:11 +0700

    Update8-9

---
+++ Lab09_Ex09/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Circle.PrintCircleArea(100);
+ class Circle
+   {
+    const double PI = 3.14159;
+    public static void PrintCircleArea(double radius)
+    {
+    const double PI = 3.14159265359; 
+    System.Console.WriteLine($"Radius = {radius}, Area = {PI * radius * radius}");
+    }
+   }
